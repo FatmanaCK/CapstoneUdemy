925971abd9f54b215ee90e532f2cfa9b5e847bf1
FatmanaCK <fatmana.citis@hcl.com>
2021-11-14 21:45:34 -0600

    branch40 updateProjectTask

--- demo/src/main/java/com/fatmana/capstoneproject/web/BacklogController.java
+++ demo/src/main/java/com/fatmana/capstoneproject/web/BacklogController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
-
 @RestController
 @RequestMapping("/api/backlog")
 @CrossOrigin
@@ -51,4 +50,19 @@ public class BacklogController {
         ProjectTask projectTask = projectTaskService.findPTByProjectSequence(backlog_id, pt_id);
         return new ResponseEntity<ProjectTask>( projectTask, HttpStatus.OK);
     }
+
+
+    @PatchMapping("/{backlog_id}/{pt_id}")
+    public ResponseEntity<?> updateProjectTask(@Valid @RequestBody ProjectTask projectTask, BindingResult result,
+                                               @PathVariable String backlog_id, @PathVariable String pt_id ){
+
+        ResponseEntity<?> errorMap = mapValidationErrorService.MapValidationService(result);
+        if (errorMap != null) return errorMap;
+
+        ProjectTask updatedTask = projectTaskService.updateByProjectSequence(projectTask,backlog_id,pt_id);
+
+        return new ResponseEntity<ProjectTask>(updatedTask,HttpStatus.OK);
+
+    }
+
 }
