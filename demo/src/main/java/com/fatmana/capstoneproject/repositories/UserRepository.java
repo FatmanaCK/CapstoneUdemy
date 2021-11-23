f811d27d8fcfa783ee7c2f28d500b0a30de1a4b1
FatmanaCK <fatmana.citis@hcl.com>
2021-11-23 17:52:53 -0600

    branch 64 JWT provider pre-work

--- demo/src/main/java/com/fatmana/capstoneproject/repositories/UserRepository.java
+++ demo/src/main/java/com/fatmana/capstoneproject/repositories/UserRepository.java
@@ -5,6 +5,16 @@ import com.fatmana.capstoneproject.domain.User;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+
+
 @Repository
 public interface UserRepository extends CrudRepository<User, Long> {
+
+
+    User findByUsername(String username);
+
+    User getById(Long id);
+
+
+
 }
