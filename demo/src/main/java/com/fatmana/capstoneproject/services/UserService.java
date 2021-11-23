7df0f0a19d6c1cacd5be0901dc42899f63966803
FatmanaCK <fatmana.citis@hcl.com>
2021-11-23 12:32:31 -0600

    branch 62 custom user

--- demo/src/main/java/com/fatmana/capstoneproject/services/UserService.java
+++ demo/src/main/java/com/fatmana/capstoneproject/services/UserService.java
@@ -1,6 +1,7 @@
 package com.fatmana.capstoneproject.services;
 //you guessed it up we can handle the registration so that we can actually save and update
 
+import com.fatmana.capstoneproject.exceptions.UsernameAlreadyExistsException;
 import com.fatmana.capstoneproject.repositories.UserRepository;
 import org.apache.catalina.User;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,13 +19,19 @@ public class UserService {
     private BCryptPasswordEncoder bCryptPasswordEncoder;
 
     public User saveUser (User newUser){
-        newUser.setPassword(bCryptPasswordEncoder.encode(newUser.getPassword()));
 
-        //Username has to be unique (exception)
+        try{
+            newUser.setPassword(bCryptPasswordEncoder.encode(newUser.getPassword()));
+            //Username has to be unique (exception)
+            newUser.setUsername(newUser.getUsername());
+            // Make sure that password and confirmPassword match
+            // We don't persist or show the confirmPassword
+            return userRepository.save(newUser);
 
-        // Make sure that password and confirmPassword match
-        // We don't persist or show the confirmPassword
-        return userRepository.save(newUser);
+        }catch (Exception e){
+            throw new UsernameAlreadyExistsException("Username '"+newUser.getUsername()+"' already exists");
+        }
+
     }
 
 
